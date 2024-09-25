16637ed009369ae89271a943d5eb4146abbd3933
GustavoBS-PIXEL <gustavosilvino76458@gmail.com>
2024-09-25 20:03:38 -0300

    Initial commit

---
+++ src/main/java/br/edu/famper/api_sistemacinema/ApiSistemaCinemaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.famper.api_sistemacinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSistemaCinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSistemaCinemaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/famper/api_sistemacinema/ApiSistemaCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.famper.api_sistemacinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSistemaCinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
