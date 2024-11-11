3db29c3e17556e73920b47658d519006b0c3e29a
GustavoBS-PIXEL <gustavosilvino76458@gmail.com>
2024-11-11 19:35:56 -0300

    Final Service2 commit

--- src/main/java/br/edu/famper/api_sistemacinema/repository/SessaoRepository.java
+++ src/main/java/br/edu/famper/api_sistemacinema/repository/SessaoRepository.java
@@ -1,6 +1,5 @@
 package br.edu.famper.api_sistemacinema.repository;
 
-import br.edu.famper.api_sistemacinema.model.SalaCinema;
 import br.edu.famper.api_sistemacinema.model.Sessao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
